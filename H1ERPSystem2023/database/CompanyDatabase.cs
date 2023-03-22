213254ee119306eae6e23ccdb67ad4786d2dee61
KindaTiny <Lukas@skagennet.dk>
2023-03-22 15:13:01 +0100

    B1 : CompanyDatabase Added Metoder Skrevet ned, Mangler At hente Companys

---
+++ H1ERPSystem2023/database/CompanyDatabase.cs
@@ -0,0 +1,37 @@
+﻿using H1_ERP_System_2023.Domain_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP_System_2023.Database
+{
+    internal partial class Database
+    {
+        public string Company { get; set; }
+        
+        public CompanyModel GetCompany()
+        {
+            return Company;
+        }
+        public List<CompanyModel> GetAllCompanyModels() { //foreach?
+
+            throw new NotImplementedException();
+        }
+        public List<CompanyModel> AddCompany() {
+            throw new NotImplementedException();  //List?
+        }
+        public CompanyModel UpdateCompany()
+        {
+            throw new NotImplementedException();
+
+        }
+        public List<CompanyModel> RemoveCompany()
+        
+        { throw new NotImplementedException(); } //List?
+       
+
+    }
+}
+
