745606d9e8fc3034e52221561adb2add6099ee8e
KindaTiny <Lukas@skagennet.dk>
2023-03-22 12:51:09 +0100

    Arbejde på companyModel

--- H1ERPSystem2023/Domain Model/CompanyModel.cs
+++ H1ERPSystem2023/Domain Model/CompanyModel.cs
@@ -2,5 +2,12 @@
 {
     internal class CompanyModel
     {
+        public string ItemNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int salgspris { get; set; }
+        public string Location { get; set; } // nummer på 4 bogstaver, 
+        public int Quantity { get; set; }
+        public string Measure { get; set; }
     }
 }
