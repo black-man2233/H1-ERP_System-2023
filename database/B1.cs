f524c38b702394219cd36ed558f09004a1a7ebe3
KindaTiny <Lukas@skagennet.dk>
2023-03-22 11:20:52 +0100

    Database B1

---
+++ database/B1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP_System_2023.Database
+{
+    internal class B1
+    {
+        private SqlConnection getConnection()
+        {
+            SqlConnectionStringBuilder sb = new();
+            sb.DataSource = "docker.data.techcollege.dk";
+            sb.InitialCatalog = "H1PD021123_Gruppe4";
+            sb.UserID = "H1PD021123_Gruppe4";
+            sb.Password = "H1PD021123_Gruppe4";
+            string connectionString = sb.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
