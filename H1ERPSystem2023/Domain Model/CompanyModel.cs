4eeb06ee1428663b80338c0866671c0523245d4e
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-03-22 13:46:36 +0100

    created "SalesOrder" (D6) and filled "CompanyModel" (D1)

--- H1ERPSystem2023/Domain Model/CompanyModel.cs
+++ H1ERPSystem2023/Domain Model/CompanyModel.cs
@@ -1,6 +1,14 @@
 ﻿namespace H1ERPSystem2023.DomainModel
 {
-    internal class CompanyModel
+    public enum Currency { DKK, USD }
+    public class CompanyModel
     {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public Currency Currency { get; set; }
     }
 }
