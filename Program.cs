ce6ba88d82d6ff33bce8d9bdd9162366f811d021
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-03-22 09:55:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace H1_ERP_System_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
