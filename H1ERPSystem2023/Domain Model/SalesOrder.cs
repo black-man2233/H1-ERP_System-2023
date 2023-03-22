8ae4430ea09cdbffaa3aaf2c29867cdb9c65277a
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-03-22 13:49:31 +0100

    D6 Updated

--- H1ERPSystem2023/Domain Model/SalesOrder.cs
+++ H1ERPSystem2023/Domain Model/SalesOrder.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace H1_ERP_System_2023.Domain_Model
 {
-    internal class SalesOrder
+
+    public enum Condition
     {
+        None,
+        Created,
+        Confirmed,
+        Packed,
+        Done
     }
+    public class SalesOrder
+    {
+        public int OrderNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime CompleteDate { get; set; }
+        public int CustomerID { get; set; }
+        public Condition Condition { get; set; }
+
+
+    }
+    public class OrderSum
+    {
+        public double Sum { get; set; }
+
+    }
+
 }
+
