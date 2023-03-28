60e3640aaed10ad269eb42c0bb357ad3779dbddc
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-03-28 10:25:35 +0200

    Usings Fix

--- H1ERPSystem2023/Program.cs
+++ H1ERPSystem2023/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace H1ERPSystem2023
+﻿using H1ERPSystem2023.Databasefiles;
+using H1ERPSystem2023.DomainModel;
+using H1ERPSystem2023.Screens;
+using TECHCOOL.UI;
+
+namespace H1ERPSystem2023
 {
     internal class Program
     {
@@ -6,7 +11,7 @@
         {
             Console.WriteLine("Hello, World!");
             Database db = new Database();
-            SalesOrder salesOrder = new SalesOrder()
+            SalesOrderModel salesOrder = new()
             {
                 OrderNumber = 1,
                 CreationDate = new DateTime(),
