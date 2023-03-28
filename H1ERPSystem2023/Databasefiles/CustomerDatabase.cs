de44f824e7692abe4ad0b693512e94a584ee0ef1
KindaTiny <Lukas@skagennet.dk>
2023-03-28 10:30:25 +0200

    k2: Customer.cs Givet mulighed for Null: Customer Database Lavet 2 Customers

---
+++ H1ERPSystem2023/Databasefiles/CustomerDatabase.cs
@@ -0,0 +1,38 @@
+﻿using H1ERPSystem2023.Domain_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace H1_ERP_System_2023.Databasefiles
+{
+    public partial class CustomerDatabase
+    {
+        private List<Customer> Customers = new List<Customer>();
+
+        void _addCompanies()
+        {
+            Customers.Add(new Customer(1, "FirstName", "LastName", null, "12345678", "Email@Email.com", "1", null));
+            Customers.Add(new Customer(2, "AnotherGuy", "fafaf", null, "87654321", "AG@mail.com", "2", null));
+        }
+
+        public CustomerDatabase()   
+        {
+            _addCompanies();
+        }
+    }
+
+
+public class MyFirstScreen : Screen
+    {
+        public override string Title { get; set; } = "My first screen";
+        protected override void Draw()
+        {
+            Clear(this);
+            Console.WriteLine("My first screen232322!");
+        }
+    }
+}
+
--- H1ERPSystem2023/Domain Model/CustomerModel.cs
+++ H1ERPSystem2023/Domain Model/CustomerModel.cs
@@ -1,14 +1,14 @@
-﻿namespace H1ERPSystem2023.DomainModel
+﻿namespace H1ERPSystem2023.Domain_Model
 {
-    internal class CustomerModel : PersonModel
+    internal class Customer : Person
     {
         public string CustomerNumber { get; set; }
-        public DateTime LastPurchaseDate { get; set; }
+        public DateTime? LastPurchaseDate { get; set; }
 
         /// <summary>
         /// Creates a new Customer With a Person Parameters
         /// </summary>
-        public CustomerModel(int personId, string firstName, string lastName, AddressModel address, string phoneNumber, string emailAddress, string customerNumber, DateTime lastPurchaseDate) : base(personId, firstName, lastName, address, phoneNumber, emailAddress)
+        public Customer(int personId, string firstName, string lastName, Address? address, string phoneNumber, string emailAddress, string customerNumber, DateTime? lastPurchaseDate) : base(personId, firstName, lastName, address, phoneNumber, emailAddress)
         {
             #region Person
             this.PersonId = personId;
