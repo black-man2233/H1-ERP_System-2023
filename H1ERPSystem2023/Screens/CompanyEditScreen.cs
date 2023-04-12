37efcd7e459c0fd8b3d79dd24ec6fc4ed3c66466
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2023-04-12 09:10:27 +0200

    V3 Push

--- H1ERPSystem2023/Screens/CompanyEditScreen.cs
+++ H1ERPSystem2023/Screens/CompanyEditScreen.cs
@@ -1,30 +1,40 @@
 ﻿using H1ERPSystem2023.Databasefiles;
 using H1ERPSystem2023.DomainModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TECHCOOL.UI;
 
 namespace H1ERPSystem2023.Screens
 {
     public class CompanyEditScreen : Screen
     {
+
         public override string Title { get; set; } = "Edit Company";
 
         protected override void Draw()
         {
-            //CompanyModel compModel = new CompanyModel(1, "Virksomhed", "Vejej", "Nummer", "9900", "Aalborg,", "Denmark", Currency.DKK);
-            //Delete above and use the Company Database so data is more consistent
-            CompanyDatabase compDB = new();
+            Clear(this);
 
-            Form <CompanyModel> editor = new();
+            ListPage<CompanyModel> compList = new();
+            Form<CompanyModel> editor = new();
+            Database compDB = new();
+            CompanyModel company;
 
+            foreach (CompanyModel compModel in compDB.Companies)
+                compList.Add(compModel);
+            compList.AddColumn("navn", "CompanyName");
+            //compList.AddColumn("");
+            company = compList.Select();
+           
             editor.TextBox("Company Name", "CompanyName");
-            editor.TextBox("2", "CompanyName");
+            editor.TextBox("Street", "Street");
+            editor.TextBox("StreetNumber", "StreetNumber");
+            editor.TextBox("Country", "Country");
+            editor.TextBox("Currency", "Currency");
 
-            //editor.Edit(compModel);
+
+
+
+            editor.Edit(company);
+            Console.ReadLine();
             //Use Company Database ^
         }
     }
