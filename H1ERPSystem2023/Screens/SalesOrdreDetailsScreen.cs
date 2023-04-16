75ef8af1741ca663147e85dfcc927c1109947194
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-16 20:01:12 +0200

    Sales order details screen created

---
+++ H1ERPSystem2023/Screens/SalesOrdreDetailsScreen.cs
@@ -0,0 +1,17 @@
+﻿using H1ERPSystem2023;
+using H1ERPSystem2023.Databasefiles;
+using H1ERPSystem2023.DomainModel;
+using TECHCOOL.UI;
+
+namespace H1_ERP_System_2023.Screens
+{
+    public class SalesOrdreDetailsScreen : Screen
+    {
+        protected override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Title { get; set; }
+    }
+}
