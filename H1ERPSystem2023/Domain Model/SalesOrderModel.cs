e768b9538f7c53eeb5a7a7d8fac78a557326233f
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-16 20:18:52 +0200

    * Created a constructor, and a property that returns the Customers full name

--- H1ERPSystem2023/Domain Model/SalesOrderModel.cs
+++ H1ERPSystem2023/Domain Model/SalesOrderModel.cs
@@ -1,6 +1,7 @@
-﻿namespace H1ERPSystem2023.DomainModel
-{
+﻿using H1ERPSystem2023.Databasefiles;
 
+namespace H1ERPSystem2023.DomainModel
+{
     public enum Condition
     {
         None,
@@ -9,31 +10,73 @@
         Packed,
         Done
     }
+
     public class SalesOrderModel
     {
         public int OrderNumber { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime CompleteDate { get; set; }
-        public int CustomerID { get; set; }
+        public DateTime? CompleteDate { get; set; }
+        public string CustomerID { get; set; }
+
+        public string CustomerName
+        {
+            get
+            {
+                //Loops through all the customers
+                foreach (CustomerModel customer in Database.Instance.GetCuStomersList())
+                {
+                    //returns the customers name, if the this.CustomerId matches the customerid
+                    if (this.CustomerID == customer.CustomerNumber)
+                    {
+                        return customer.FullName;
+                    }
+                }
+
+                return null;
+            }
+        }
+
         public Condition Condition { get; set; }
         public List<OrderLineModel> OrderLines { get; }
+
+        public decimal Amount
+        {
+            get => sum();
+        }
 
         /// <summary>
         /// Gives a sum of Product
         /// </summary>
         /// <returns>The sum of all the product chosen</returns>
-        public double Sum()
+
+        #region Constructors
+
+        public SalesOrderModel()
         {
-            double sum = 0;
+            //used to create a sale order, with no data
+        }
+
+        public SalesOrderModel(int orderNumber, string customerId, Condition condition, List<OrderLineModel> orderLines)
+        {
+            this.OrderNumber = orderNumber;
+            this.CreationDate = DateTime.Today;
+            this.CompleteDate = null;
+            this.CustomerID = customerId;
+            this.Condition = condition;
+            this.OrderLines = orderLines;
+        }
+
+        #endregion
+
+        private decimal sum()
+        {
+            decimal sum = 0;
             foreach (OrderLineModel line in OrderLines)
             {
-
                 sum = sum + line.Product.SalePrice;
             }
+
             return sum;
         }
     }
-
-
 }
-
