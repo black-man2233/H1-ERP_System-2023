719c2fbc6c4a0d5c633ba5d91122459d69342650
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-16 19:54:01 +0200

    CustomerModel class wasnt accessible cause it was internal.

--- H1ERPSystem2023/Domain Model/CustomerModel.cs
+++ H1ERPSystem2023/Domain Model/CustomerModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace H1ERPSystem2023.DomainModel
 {
-    internal class CustomerModel : PersonModel
+    public class CustomerModel : PersonModel
     {
         public string CustomerNumber { get; set; }
         public DateTime LastPurchaseDate { get; set; }
@@ -8,23 +8,27 @@
         /// <summary>
         /// Creates a new Customer With a Person Parameters
         /// </summary>
-        public CustomerModel(int personId, string firstName, string lastName, AddressModel address, string phoneNumber, string emailAddress, string customerNumber, DateTime lastPurchaseDate) : base(personId, firstName, lastName, address, phoneNumber, emailAddress)
+        public CustomerModel(int personId, string firstName, string lastName, AddressModel address, string phoneNumber,
+            string emailAddress, string customerNumber, DateTime lastPurchaseDate) : base(personId, firstName, lastName,
+            address, phoneNumber, emailAddress)
         {
             #region Person
+
             this.PersonId = personId;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Address = address;
             this.PhoneNumber = phoneNumber;
             this.EmailAddress = emailAddress;
+
             #endregion
 
             #region Customer
+
             this.CustomerNumber = customerNumber;
             this.LastPurchaseDate = lastPurchaseDate;
+
             #endregion
         }
-
-
     }
 }
