b57cfc5461202182ad13157a7203e3ae36a5dd52
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-17 13:03:46 +0200

    Deleted unexisted method

--- H1ERPSystem2023/Screens/MenuScreen.cs
+++ H1ERPSystem2023/Screens/MenuScreen.cs
@@ -16,7 +16,6 @@ namespace H1ERPSystem2023.Screens
             menu.Add(new CompaniesListScreen());
             menu.Add(new CompanyEditScreen());
             menu.Add(new SalesOrderHeadersScreen());
-            menu.Add(new SalesOrdreDetailsScreen());
 
             menu.Start(this);
         }
