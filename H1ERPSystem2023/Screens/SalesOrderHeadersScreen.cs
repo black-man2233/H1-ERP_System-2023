e166482cbb5ca851fbff4c7a3fe7a56ee3261fb0
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-19 14:11:51 +0200

    S2, Sales Order Header Screen finnished

--- H1ERPSystem2023/Screens/SalesOrderHeadersScreen.cs
+++ H1ERPSystem2023/Screens/SalesOrderHeadersScreen.cs
@@ -5,38 +5,40 @@ using TECHCOOL.UI;
 
 namespace H1_ERP_System_2023.Screens
 {
+    /// <summary>
+    /// Displays All sales orders with their properties
+    /// </summary>
     public class SalesOrderHeadersScreen : Screen
     {
-        public override string Title { get; set; } = "Sales Order Headers";
+        public override string Title { get; set; } = "Sales Order Header";
         private SalesOrderModel? SelectedSaleOrder { get; set; }
-
-        private List<SalesOrderModel> _salesOrdersList = new();
-        private List<CustomerModel> _customersList = new();
 
         protected override void Draw()
         {
-            //Make sure to put in loop so user can always select
+            //loops so user can always select an item
             do
             {
                 //Clear the screen at the start to avoid other text
                 Clear(this);
 
                 ListPage<SalesOrderModel?> salesOrderList = new();
-
                 foreach (SalesOrderModel saleOrder in Database.Instance.GetSalesOrder())
+                {
                     salesOrderList.Add(saleOrder);
+                }
 
                 salesOrderList.AddColumn("Order Number", "OrderNumber");
                 salesOrderList.AddColumn("Date", "CreationDate");
-                salesOrderList.AddColumn("Custommer Number", "CustomerID");
-                salesOrderList.AddColumn("First and Last Name", "");
+                salesOrderList.AddColumn("Customer Number", "CustomerID");
+                salesOrderList.AddColumn("First and Last Name", "CustomerName");
                 salesOrderList.AddColumn("Amount", "Amount");
 
 
-                //Gives the user the option to Select between the companies when called, make sure to check for not null. That will tell it's been selected with enter
+                //option to Select between SaleOrders,
+                //make sure to check for not null. That will tell it's been selected with enter
                 SelectedSaleOrder = salesOrderList.Select();
                 if (SelectedSaleOrder != null)
-                    Screen.Display(new CompanyDetailScreen());
+                    Screen.Display(new SaleOrderDetailScreen(SelectedSaleOrder));
                 else
                 {
                     Quit();
