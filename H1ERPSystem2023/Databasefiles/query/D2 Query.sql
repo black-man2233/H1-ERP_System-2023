bb5bc8db5d1d3de014e04e707a8ca8f0481b2f58
SaltedFishy03 <seekerbrutus@gmail.com>
2023-05-04 12:25:43 +0200

    made a query with a table for products and added some values to the table

---
+++ H1ERPSystem2023/Databasefiles/query/D2 Query.sql
@@ -0,0 +1,25 @@
+﻿USE H1PD021123_Gruppe5;
+DROP TABLE IF EXISTS Products;
+
+
+CREATE TABLE Products
+(
+    ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    ProductName VARCHAR(20),
+    Descriptionn TEXT,
+    SalePrice NUMERIC,
+    BuyPrice NUMERIC,
+    Locationn VARCHAR(20),
+    StorageAmount FLOAT,
+    Avance AS SalePrice - BuyPrice,
+    AvanceProcent AS (SalePrice - BuyPrice) / BuyPrice * 100
+);
+	
+
+
+INSERT INTO Products (ProductName, Descriptionn, SalePrice, BuyPrice, Locationn, StorageAmount)
+VALUES
+('Computer', 'Good pc for work' , 4000, 2500, 'Denmark' , 20),
+('vandflasker', 'Good pc for work' , 20, 5, 'Denmark' , 200);
+
+SELECT * FROM Products;
