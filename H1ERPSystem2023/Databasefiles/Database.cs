e4b848fbefe4256798eff82d5762207191eafe75
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-12 01:48:09 +0200

    New Method of Getting data from the database

--- H1ERPSystem2023/Databasefiles/Database.cs
+++ H1ERPSystem2023/Databasefiles/Database.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿#pragma warning disable
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using H1ERPSystem2023.DomainModel;
 
 namespace H1ERPSystem2023.Databasefiles
 {
@@ -15,13 +17,22 @@ namespace H1ERPSystem2023.Databasefiles
         static Database()
         {
             Instance = new Database();
-            Instance._addCompanies();
-            Instance._addProducts();
-            Instance._addCustomers();
-            Instance._addSalesOrders();
+            GetAllData(GetConnection());
         }
 
-     
+        
+        private static void GetAllData(SqlConnection connection)
+        {
+            try
+            {
+                GetAddressesFromDB(connection);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong while trying to retrieve addesses from the database");
+            }
+        }
+
         private static SqlConnection GetConnection()
         {
             SqlConnectionStringBuilder sb = new()
